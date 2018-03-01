bb1bc7dd72b3d355f9f04e32200dc105eb823f04
Edward Guo <eddiesguo@gmail.com>
2018-02-28 22:51:18 -0800

    added link

--- src/App.js
+++ src/App.js
@@ -2,20 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav.js';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <div style={{flex:'25%', borderStyle:'solid', textAlign:'center', float: 'left'}}>
-          <Nav />
+      <Router>
+        <div>
+          <div style={{flex:'25%', borderStyle:'solid', textAlign:'center', float: 'left'}}>
+            <Nav />
+          </div>
+          <div style={{ flex:'75%'}}>
+            <h1>hiii</h1>
+            <hr />
+            <Link to="/">newpage</Link>
+            <Link to="A">newpage</Link>
+            <Route path="/" component={About} />
+            <Route path="/A" component={Home}/>
+          </div>
         </div>
-        <div style={{ flex:'75%'}}>
-          <h1>COL2</h1>
-        </div>
-      </div>
+      </Router>
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+const About = () => (
+  <div>
+    <h2>About HI STUFF GOES HERE</h2>
+  </div>
+);
 
 export default App;
