bbd407efa3261003e1351a71c60147d937f9e559
Zhe Wang <zhw199@ucsd.edu>
2018-03-04 13:37:27 -0500

    The box is too deep.

--- src/CompletedProject.test.js
+++ src/CompletedProject.test.js
@@ -7,7 +7,7 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() })
 
-describe('Completed Project via Enzyme', () => {
+describe('Compelted Project via Enzyme', () => {
 
 
   it('Completed Project page contains an id projects', () => {
@@ -16,18 +16,36 @@ describe('Completed Project via Enzyme', () => {
     expect(wrapper.find('#projects').length).toBe(1);
   });
 
-  it('Completed Project page contains projects', () => {
+  it('Completed Project page contains an id projets ', () => {
     const wrapper = shallow(<CompletedProject />);
     expect(wrapper.find(GetCompletedProjects).length).toBe(1);
-    //expect(wrapper.find(CompletedProjectBox).length).toBe(1);
   });
 
-  it('Completed Project page contains projects', () => {
-    const wrapper = shallow(<CompletedProjectBox />);
-    expect(wrapper.find('.active_project').length).toBe(1);
-    //expect(wrapper.find(CompletedProjectBox).length).toBe(1);
-  });
+  it('Completed Project page contains an id projets ', () => {
+    var state = {projects : [{
+    projectID :1234,
+    projectName: "New Hair",
+    budget : 150,
+    endDate: "2/12/2018",
+    stylist: "Jason",
+    associate: "Eddie",
+    key:123
+  },
+  {
+  projectID :12345,
+  projectName: "New Hair",
+  budget : 150,
+  endDate: "2/12/2018",
+  stylist: "Jason",
+  associate: "Eddie",
+  key:12345
+}
 
+]};
+    const wrapper = shallow(<GetCompletedProjects data = {state.projects}/>);
+    expect(wrapper.find(CompletedProjectBox).length).toBe(2);
+
+  });
 
 
 });
